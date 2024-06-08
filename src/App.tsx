0fba7d522f01b50645bfbd4ed69bc9f696557643
Yonatan Rozin <yonatanrozin42@gmail.com>
2024-06-08 02:58:08 -0400

    update studio

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,21 @@ import Tract, { RPT_Voice } from './RPT/RPT'
 import RecordingStudio, {Recording} from './RecordingStudio';
 
 import './App.css'
+import MPT from "./RPT/pink_trombone_processor.js?worker&url";
 
 export default function App() {
 
   const [voice, setVoice] = useState<RPT_Voice>();
   const [ctx, setCtx] = useState<AudioContext>();
   const [isRecording, setIsRecording] = useState(false);
-  const [completeRecording, setCompleteRecording] = useState<Recording>(); //holds processed recording
   const [voiceMute, setVoiceMute] = useState(true);
   const [gainNode, setGainNode] = useState<GainNode>();
+
+  const [recordings, setRecordings] = useState<Record<string, Recording>>({});
+  const [currentRecording, setCurrentRecording] = useState<string>();
   
   const recIntervalRef = useRef<number>();
   const recordingRef = useRef<Recording>(); //holds the in-progress recording frames
-  const tractCanvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     const newCtx = new AudioContext();
@@ -37,7 +39,7 @@ export default function App() {
   useEffect(() => {
     if (!ctx) return;
     (async () => {
-      await ctx.audioWorklet.addModule(new URL('./RPT/pink_trombone_processor.js', import.meta.url));
+      await ctx.audioWorklet.addModule(MPT);
       console.log("Modules added successfully.");
       setVoice(new RPT_Voice("studio", ctx, gainNode));
     })();
@@ -46,7 +48,8 @@ export default function App() {
   useEffect(() => {
     if (!voice) return;
     voice.connect(); 
-    (window as any).voice = voice
+    voice.glottis.parameters.get("intensity")!.value = 1;
+    (window as any).voice = voice;
   }, [voice]);
 
   useEffect(() => {
@@ -59,7 +62,7 @@ export default function App() {
     recIntervalRef.current = setInterval(recordFrame, 1000/120);
     voice.tract.parameters.get("movement-speed")!.value = -1;
     voice.UI.ignoreTongue = true;
-    recordingRef.current = {frames: []};
+    recordingRef.current = [];
   }
 
   function recordFrame() {
@@ -67,14 +70,15 @@ export default function App() {
 
     const n = voice.tract.parameters.get("n")!.value;
 
-    if (voice.constriction) recordingRef.current!.frames.push({
-      ci: Number(voice.constriction.i.toFixed(3))/n * 44,
-      cd: Number(voice.constriction.d.toFixed(3)),
+    recordingRef.current!.push({
+      ci: Number((voice.tract.parameters.get("constriction-index")!.value/n * 44).toFixed(3)),
+      cd: Number(voice.tract.parameters.get("constriction-diameter")!.value.toFixed(3)),
+      ti: Number((voice.tract.parameters.get("tongue-index")!.value/n * 44).toFixed(3)),
+      td: Number(voice.tract.parameters.get("tongue-diameter")!.value.toFixed(3)),
       v: Number(voice.tract.parameters.get("velum-target")!.value.toFixed(3)),
       t: 1,
       i: 1,
-      n: voice.constriction? 1 : 0,
-      ta: 0
+      n: 1,
     });
   }
   
@@ -83,35 +87,91 @@ export default function App() {
     clearInterval(recIntervalRef.current);
     voice.UI.ignoreTongue = false;
     voice.tract.parameters.get("movement-speed")!.value = voice.tract.parameters.get("movement-speed")!.defaultValue;
-
-    if (completeRecording && !window.confirm("Overwrite existing recording?")) return;
     processRecording(recordingRef.current!);
   }
 
   function processRecording(rec: Recording) {
-    for (let i = rec.frames.length - 1; i >= 0; i--) {
-      if (JSON.stringify(rec.frames[i-1]) == JSON.stringify(rec.frames[i])) {
-        rec.frames.splice(i, 1);
+    for (let i = rec.length - 1; i >= 0; i--) {
+      if (JSON.stringify(rec[i-1]) == JSON.stringify(rec[i])) {
+        rec.splice(i, 1);
       }
     }
-    setCompleteRecording({...rec});
+    let recName = window.prompt("Enter recording name:")?.trim();
+    if (recName) {
+      recordings[recName] = rec;
+      setRecordings(recordings);
+      setCurrentRecording(recName);
+    }
   }
 
   useEffect(() => {
-    if (gainNode && voice) gainNode.gain.setTargetAtTime(voiceMute ? 0 : 1, voice.ctx.currentTime, 0.05)
-    }, [voiceMute, voice]);
+    if (gainNode && voice) gainNode.gain.setTargetAtTime(voiceMute ? 0 : 1, voice.ctx.currentTime, 0.05);
+  }, [voiceMute, voice]);
+
+  function handleJSONFile(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0];
+    if (!file) return;
+    const reader = new FileReader();
+    reader.onload = readerEvent => {
+        var content = readerEvent.target?.result?.toString();
+        if (content) {
+            const JSONIn = JSON.parse(content) as Record<string, Recording>;
+            setRecordings(JSONIn); 
+            setCurrentRecording(Object.keys(JSONIn)[0]);
+        }
+    }
+    reader.readAsText(file);
+  }
+
+  function download(content: {}) {
+    const a = document.createElement("a");
+    const file = new Blob([JSON.stringify(content)], { type: "application/JSON" });
+    a.href = URL.createObjectURL(file);
+    a.download = "recordings.json";
+    a.click();
+  }
 
   return <div>
     <h1>Pink Trombone Studio</h1>
     <h3>By Yonatan Rozin</h3>
     <h5>Based on <a href='https://dood.al/pinktrombone/'>Pink Trombone</a> by Neil Thapen (MIT License)</h5>
-    <p>Hold SPACE to record speech shapes using the vocal tract UI below. </p>
-    <div style={{display: "flex"}}>
-      <label>Mute voice
-        <input type="checkbox" checked={voiceMute} onChange={e => setVoiceMute(e.target.checked)}/>
-      </label>
-      {voice && <Tract voice={voice} canvasRef={tractCanvasRef}/>}
-      {<RecordingStudio recording={completeRecording} set={setCompleteRecording} voice={voice}/>}
+    <p>Hold SPACE to record speech shapes using the vocal tract UI below.</p>
+    <div id="studio">
+      <div >
+        <label>Mute voice
+          <input type="checkbox" checked={voiceMute} onChange={e => {setVoiceMute(e.target.checked); voice?.ctx.resume()}}/>
+        </label><br/>
+        {voice && <Tract voice={voice}/>}
+      </div>
+
+      <div id="editor">
+        <div id="recSelect" style={{display: "flex", padding: "10px", gap: "10px"}}>
+          <div style={{width: "50%"}}>
+            <label>Recording:
+              <select value={currentRecording} onChange={e => setCurrentRecording(e.target.value)}>
+                {Object.keys(recordings).length == 0 && <option value={undefined} disabled selected>No recordings saved</option>}
+
+                {Object.keys(recordings).map(r =>
+                  <option key={`recording${r}`} value={r}>{r}</option>
+                  )}
+              </select>
+            </label>
+          </div>
+          <div>OR</div>
+          <div>
+            <label>Import JSON file<br />
+              <input type="file" accept=".json" onChange={handleJSONFile}/>
+            </label><br/><br/>
+          </div>
+          
+        </div>
+        {currentRecording && <RecordingStudio setRecordings={setRecordings} voice={voice} recordings={recordings}
+          currentRecording={currentRecording}/>}
+        {currentRecording && <button onClick={() => download(recordings)}>Download JSON</button>
+      }
+        <br/>
+      </div>
+
     </div>
   </div>
 }
--- src/RecordingStudio.tsx
+++ src/RecordingStudio.tsx
@@ -2,37 +2,36 @@ import { useCallback, useEffect, useRef, useState } from "react";
 import { map } from "./utils";
 import { RPT_Voice } from "./RPT/RPT";
 
-export type Recording = {
-    frames: Recording_Frame[], 
-    adj?: {i: number, d: number}
-};
+export type Recording = Recording_Frame[];
 export type Recording_Frame = {
-  ci: number, // constriction index
-  cd: number, // constriction diameter
-  v: number, //velum target (0.01 - 0.4)
-  t: number, //voice tenseness multiplier (0-1)
-  i: number, //voice intensity (0-1)
-  n: number, //noise volume (0-1)
-  ta: number //tongue adjustment (0-1)
+	ci: number, // constriction index
+	cd: number, // constriction diameter
+	ti: number,
+	td: number,
+	v: number, //velum target (0.01 - 0.4)
+	t: number, //voice tenseness multiplier (0-1)
+	i: number, //voice intensity (0-1)
+	n: number, //noise volume (0-1)
 }
 
 export const fProps: Record<string, {min: number, max: number, color: string, name: string}> = {
-    ci: {min: 0, max: 44, color: "green", name: "Constriction Index"},
-    cd: {min: 0, max: 3.5, color: "blue", name: "Constriction Diameter"},
+    ci: {min: 0, max: 44, color: "rgb(0, 0, 100)", name: "Constriction Index"},
+    cd: {min: 0, max: 3.5, color: "rgb(0, 0, 150)", name: "Constriction Diameter"},
+	ti: {min: 12, max: 20, color: "rgb(0, 100, 0)", name: "Tongue Index"},
+    td: {min: 2.05, max: 3.5, color: "rgb(0, 150, 0)", name: "Tongue Diameter"},
     i: {min: 0, max: 1, color: "red", name: "Intensity"},
     t: {min: 0, max: 1, color: "orange", name: "Tenseness"},
     v: {min: 0.01, max: 0.4, color: "purple", name: "Velum Target"},
     n: {min: 0, max: 1, color: "magenta", name: "Noise"},
-    ta: {min: 0, max: 1, color: "limegreen", name: "Tongue Adjustment"}
 }
 
 export default function RecordingStudio(props: {
-    recording?: Recording, set: React.Dispatch<React.SetStateAction<Recording | undefined>>, 
-    voice?: RPT_Voice
+	currentRecording: string, setRecordings: React.Dispatch<React.SetStateAction<Record<string, Recording>>>, 
+    voice?: RPT_Voice, recordings: Record<string, Recording>
 }) {
 
-    const {recording, set, voice} = props;
-
+    const {recordings, setRecordings, voice, currentRecording} = props;
+	
     const [mouseStart, setMouseStart] = useState<{x: number, y: number, val: number}>();
     const [mousePos, setMousePos] = useState<[number, number]>();
     const [shiftHeld, setShiftHeld] = useState(false);
@@ -43,9 +42,16 @@ export default function RecordingStudio(props: {
     const cnvRef = useRef<HTMLCanvasElement>(null);
     const intervalRef = useRef<number>();
 
+	function set(rec: Recording) {
+		recordings[currentRecording] = rec;
+        setRecordings(recordings);
+	} 
+
+    const recording = recordings[currentRecording];
+
     function reverseRecording() {
-        recording?.frames.reverse();
-        set({...recording!});
+        recording.reverse();
+        set(recording);
     }
 
     useEffect(() => {
@@ -75,8 +81,8 @@ export default function RecordingStudio(props: {
     }, [recording, frameObj]);
     
     useEffect(() => {
-        if (recording) setCurrentFrame(Math.min(Math.max(0, currentFrame), recording.frames.length - 1));
-        setFrameObj(recording?.frames[currentFrame]);
+        if (recording) setCurrentFrame(Math.min(Math.max(0, currentFrame), recording.length - 1));
+        setFrameObj(recording?.[currentFrame]);
         if (cnvRef.current && recording) draw(recording, cnvRef.current);
     }, [recording, cnvRef.current, pEditing, currentFrame, JSON.stringify(frameObj), mousePos]);
 
@@ -99,10 +105,10 @@ export default function RecordingStudio(props: {
             ctx.lineWidth = prop === pEditing ? 5 : .5;
             
             ctx.beginPath();
-            for (let i = 0; i < rec.frames.length; i++) {
-                const propVal = rec.frames[i][p];
+            for (let i = 0; i < rec.length; i++) {
+                const propVal = rec[i][p];
 
-                const x = i/(rec.frames.length - 1) * width;
+                const x = i/(rec.length - 1) * width;
                 const y = map(propVal, pInfo.min, pInfo.max, height-10, 10);
                 
                 if (i) ctx.lineTo(x, y); 
@@ -111,15 +117,15 @@ export default function RecordingStudio(props: {
             ctx.stroke();
 
             if (frameObj) {
-                const x = currentFrame/(rec.frames.length - 1) * width;
+                const x = currentFrame/(rec.length - 1) * width;
                 const y = map(frameObj[p], fProps[p].min, fProps[p].max, height-10, 10);
                 ctx.fillText(frameObj[p].toFixed(2), x < width-100? x+5 : x-75, y > 30 ? y-5 : y + 30);
             }
         }
 
-        const frameWidth = width / rec.frames.length;
+        const frameWidth = width / rec.length;
         ctx.fillStyle = "rgba(0, 0, 0, .3)";
-        ctx.fillRect(currentFrame/(rec.frames.length - 1) * width - frameWidth/2, 0, width/(rec.frames.length), height);
+        ctx.fillRect(currentFrame/(rec.length - 1) * width - frameWidth/2, 0, width/(rec.length), height);
 
         if (mousePos && pEditing) {
             ctx.globalAlpha = .5;
@@ -139,10 +145,10 @@ export default function RecordingStudio(props: {
         if (x < 0.03) x = 0; else if (x > .97) x = 1;
         if (y < 0.1) y = 0; else if (y > .9) y = 1;
 
-        const i = Math.round(x * (recording.frames.length - 1));
+        const i = Math.round(x * (recording.length - 1));
         const val = map(y, 1, 0, fProps[p].min, fProps[p].max);
-        recording.frames[i][p] = Number(val.toFixed(3));
-        set({...recording});
+        recording[i][p] = Number(val.toFixed(3));
+        set(recording);
         setMouseStart({x, y, val});
     }
 
@@ -167,39 +173,38 @@ export default function RecordingStudio(props: {
         if (y < 0.03) y = 0; else if (y > .97) y = 1;
         if (x < 0.03) x = 0; else if (x > .97) x = 1;
 
-        const i1 = Math.round(mouseStart.x * (recording.frames.length - 1));
-        const i2 = Math.round(x * (recording.frames.length - 1));
+        const i1 = Math.round(mouseStart.x * (recording.length - 1));
+        const i2 = Math.round(x * (recording.length - 1));
 
         const val = map(y, 1, 0, fProps[p].min, fProps[p].max);
 
-        if (i1 === i2) recording.frames[i1][p] = Number(val.toFixed(3));
+        if (i1 === i2) recording[i1][p] = Number(val.toFixed(3));
 
         else {
             for (let i = Math.min(i1, i2); i <= Math.max(i1, i2); i++) {
-                recording.frames[i][p] = Number(map(i, i1, i2, mouseStart.val, val).toFixed(3));
+                recording[i][p] = Number(map(i, i1, i2, mouseStart.val, val).toFixed(3));
             }
         }
 
-        set({...recording});
+        set(recording);
 
         if (!shiftHeld) setMouseStart({x, y, val});
         
     }
 
     function extendFrame(rec: Recording) {
-        rec.frames.splice(currentFrame, 0, {...frameObj!});
-        set({...rec});
+        rec.splice(currentFrame, 0, {...frameObj!});
+        set(rec);
     }
     function deleteFrame(rec: Recording) {
-        rec.frames.splice(currentFrame, 1);
-        setCurrentFrame(Math.min(currentFrame, rec.frames.length - 1))
-        set({...rec});
+        rec.splice(currentFrame, 1);
+        setCurrentFrame(Math.min(currentFrame, rec.length - 1))
+        set(rec);
     }
 
     useEffect(() => {
         if (frameObj && voice) {
             const n = voice.tract.parameters.get("n")!.value;
-            const adj = recording?.adj;
             voice.tract.parameters.get("constriction-index")!.value = frameObj.ci/44 * n || 0;
             voice.tract.parameters.get("constriction-diameter")!.value = frameObj.cd || 0;
             voice.glottis.parameters.get("intensity")!.value = frameObj.i;
@@ -207,75 +212,12 @@ export default function RecordingStudio(props: {
             voice.tract.parameters.get("velum-target")!.value = frameObj.v;
             voice.tract.parameters.get("fricative-strength")!.value = frameObj.n;
 
-            voice.tract.parameters.get("tongue-index")!.value = 
-                adj && frameObj.ta ? map(frameObj.ta, 0, 1, voice.tongue.i, adj.i/44 * n) : voice.tongue.i;
-                voice.tract.parameters.get("tongue-diameter")!.value = 
-                adj && frameObj.ta ? map(frameObj.ta, 0, 1, voice.tongue.d, adj.d/44 * n) : voice.tongue.d;
+            voice.tract.parameters.get("tongue-index")!.value = frameObj.ti;
+			voice.tract.parameters.get("tongue-diameter")!.value = frameObj.td;
         }
     }, [JSON.stringify(frameObj)]);
 
-    function download(content: {}) {
-        const recName = window.prompt("Enter recording name:");
-        if (!recName) return;
-        const a = document.createElement("a");
-        const file = new Blob([JSON.stringify(content)], { type: "application/JSON" });
-        a.href = URL.createObjectURL(file);
-        a.download = recName + ".json";
-        a.click();
-    }
-
-    function handleJSONFile(e: React.ChangeEvent<HTMLInputElement>) {
-        const file = e.target.files?.[0];
-        if (!file) return;
-
-        const reader = new FileReader();
-        reader.onload = readerEvent => {
-            var content = readerEvent.target?.result?.toString();
-            if (content) {
-                const JSONIn = JSON.parse(content);
-                if (validateJSON(JSONIn)) {set(JSONIn); setCurrentFrame(0)};
-            }
-        }
-        reader.readAsText(file);
-    }
-
-    function validateJSON(obj: Partial<Recording>) {
-        try {
-            if (!Array.isArray(obj.frames)) return false;
-            else {
-                for (let i = 0; i < obj.frames.length; i++) {
-                    if (!["ci", "cd", "v", "t", "i", "n", "ta"].every(p => p in obj.frames![i])) return false;
-                }
-            }
-        } catch (e) {return false};
-        return true;
-    }
-
-    function addTongueAdjustment() {
-        recording!.adj = {i: 12.7, d: 2};
-        set({...recording!});
-    }
-
-    function removeTongueAdjustment() {
-        delete recording!.adj;
-        set({...recording!});
-    }
-
-    function setTongueAdjustment(p: "i" | "d", val: number) {
-        recording!.adj![p] = val;
-        set({...recording!});
-    }
-
-    function getTongueAdjustment() {
-        recording!.adj = {i: Number(voice!.tongue.i.toFixed(3)), d: Number(voice!.tongue.d.toFixed(3))};
-        set({...recording!});
-    }
-
     return <div>
-        <label>Import JSON file<br></br>
-            <input type="file" accept=".json" onChange={handleJSONFile}/>
-        </label><br/><br/>
-        
         {recording && <div style={{display: "flex", flexDirection: "column"}}>
             <label>Property: 
                 <select id="propSelect" value={pEditing} onChange={e => setPEditing(e.target.value)}
@@ -290,28 +232,12 @@ export default function RecordingStudio(props: {
                 onMouseDown={cnvMouseDown} onMouseUp={cnvMouseUp}
                 onMouseMove={e => cnvMouseMove(e)} onContextMenu={e => e.preventDefault()}/>
             <input type="range" style={{width: "100%", margin: "0px", boxSizing: "border-box"}} 
-                min={0} max={recording.frames.length - 1} step={1}
+                min={0} max={recording.length - 1} step={1}
                 value={currentFrame} onChange={e => setCurrentFrame(Number(e.target.value))}/>
             <button onMouseDown={() => intervalRef.current = setInterval(() => extendFrame(recording), 100)}
                 onMouseUp={() => clearInterval(intervalRef.current)}>Extend current frame</button>
             <button onClick={() => deleteFrame(recording)}>Delete current frame</button>
             <button onClick={reverseRecording}>Reverse frames</button>
-            <label>Tongue Adjustment
-                {(!recording.adj) && <button onClick={addTongueAdjustment}>Add</button>}
-                {recording.adj && <div>
-                    <input value={recording.adj.i} type="number" step={0.01}
-                        onChange={e => setTongueAdjustment("i", Number(e.target.value))} />    
-                    <input value={recording.adj.d} type="number" step={0.01}
-                        onChange={e => setTongueAdjustment("d", Number(e.target.value))} />    
-                    <button onClick={getTongueAdjustment}>Use current tongue position</button>
-                    <button onClick={removeTongueAdjustment}>Remove</button>
-                </div>}
-            </label>
-
-            <div>
-                <button onClick={() => download(recording)}>Download JSON</button>
-                <button onClick={() => navigator.clipboard.writeText(JSON.stringify(recording))}>Copy JSON</button>
-            </div>
         </div>}
 
     </div>
